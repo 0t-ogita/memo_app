04071eb2427cbc60540f24200d1ef048844afc0a
github_main <katatatibasami@gmail.com>
2024-01-25 15:38:40 +0900

    DB利用 PostgreSQL使用

--- memo.rb
+++ memo.rb
@@ -1,12 +1,40 @@
 require 'cgi'
+require 'pg' 
 require 'sinatra'
 require 'sinatra/reloader'
 
-FILE_PATH = 'public/memos.json'.freeze
-FILE_PATH.freeze
-
 def get_memos(file_path)
   File.open(file_path) { |f| JSON.parse(f.read) }
+end
+
+def conn
+  @conn ||= PG.connect( dbname: 'postgres', user: 'postgres', password: 'ogiogi' )
+end
+
+def read_memo(id)
+  result = conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
+  result.tuple_values(0)
+end
+
+def post_memo(title, content)
+  conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
+end
+
+def edit_memo(title, content, id)
+  conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3;', [title, content, id])
+end
+
+def delete_memo(id)
+  conn.exec_params('DELETE FROM memos WHERE id = $1;', [id])
+end
+
+def read_memos
+  conn.exec('SELECT * FROM memos')
+end
+
+configure do
+  result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
+  conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)') if result.values.empty?
 end
 
 get '/' do
@@ -14,35 +42,27 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = get_memos(FILE_PATH)
+  @memos = read_memos
   erb :memos
 end
 
 get '/memos/:id/editings' do
-  memos = get_memos(FILE_PATH)
-  @title = memos[params[:id]]['title']
-  @content = memos[params[:id]]['content']
+  memo = read_memo(params[:id])
+  @title = memo[1]
+  @content = memo[2]
   erb :editings
 end
 
 patch '/memos/:id' do
   title = params[:title]
   content = params[:content]
-  memos = get_memos(FILE_PATH)
-  memos[params[:id]] = { 'title' => title, 'content' => content }
-  set_memos(FILE_PATH, memos)
+  edit_memo(title, content, params[:id])
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = get_memos(FILE_PATH)
-  memos.delete(params[:id])
-  set_memos(FILE_PATH, memos)
+  delete_memo(params[:id])
   redirect '/memos'
-end
-
-def set_memos(file_path, memos)
-  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
 end
 
 get '/memos/newies' do
@@ -50,19 +70,15 @@ get '/memos/newies' do
 end
 
 get '/memos/:id' do
-  memos = get_memos(FILE_PATH)
-  @title = memos[params[:id]]['title']
-  @content = memos[params[:id]]['content']
+  memo = read_memo(params[:id])
+  @title = memo[1]
+  @content = memo[2]
   erb :show
 end
 
 post '/memos' do
   title = params[:title]
   content = params[:content]
-  memos = get_memos(FILE_PATH)
-  max_number = memos.keys.map(&:to_i).max || 0
-  id = (max_number + 1).to_s
-  memos[id] = { 'title' => title, 'content' => content }
-  set_memos(FILE_PATH, memos)
+  post_memo(title, content)
   redirect '/memos'
 end
